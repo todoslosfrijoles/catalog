120b0ec8c1137aceee92d81061cb55249f4d9a49
p00rt <serzhooo@gmail.com>
2021-07-06 22:50:53 +0300

    fix for loop

--- script/index.js
+++ script/index.js
@@ -7,7 +7,8 @@ likeBtn.forEach((element) => {
                 element.classList.add('card__button_active');
             } else {
                 element.classList.remove('card__button_active');
-            }});
+            }
+    })
 }); 
 
 let popupBtn = document.querySelector('.add-button');
@@ -26,41 +27,40 @@ function closePopup (popUp) {
 popupBtn.addEventListener('click', openPopup);
 popupBtnClose.addEventListener('click', closePopup);
 
-const cardSection = document.querySelector('.section')
+const cardSection = document.querySelector('.section');
 
-const cardData = [{
-    URL: "./images/image1.jpg",
-    name: "The Marvel Age of Comics 1961–1978. 40th Ed",
-    Text: "Welcome to the Marvel Age of Comics, a triumphant era of comic and pop culture innovation which redefined the super hero genre."
-},
-{
-    URL: "./images/image1.jpg",
-    name: "The Marvel Age of Comics 1961–1978. 40th Ed",
-    Text: "Welcome to the Marvel Age of Comics, a triumphant era of comic and pop culture innovation which redefined the super hero genre."
-},
-{
-    URL: "./images/image1.jpg",
-    name: "The Marvel Age of Comics 1961–1978. 40th Ed",
-    Text: "Welcome to the Marvel Age of Comics, a triumphant era of comic and pop culture innovation which redefined the super hero genre."
-},
-{
-    URL: "./images/image1.jpg",
-    name: "The Marvel Age of Comics 1961–1978. 40th Ed",
-    Text: "Welcome to the Marvel Age of Comics, a triumphant era of comic and pop culture innovation which redefined the super hero genre."
-},
-{
-    URL: "./images/image1.jpg",
-    name: "The Marvel Age of Comics 1961–1978. 40th Ed",
-    Text: "Welcome to the Marvel Age of Comics, a triumphant era of comic and pop culture innovation which redefined the super hero genre."
-}
+const cardData = [
+    {
+        url: "./images/image1.jpg",
+        name: "The Marvel Age of Comics 1961–1978. 40th Ed",
+        Text: "Welcome to the Marvel Age of Comics, a triumphant era of comic and pop culture innovation which redefined the super hero genre."
+    },
+    {
+        url: "./images/image1.jpg",
+        name: "The Marvel Age of Comics 1961–1978. 40th Ed",
+        Text: "Welcome to the Marvel Age of Comics, a triumphant era of comic and pop culture innovation which redefined the super hero genre."
+    },
+    {
+        url: "./images/image1.jpg",
+        name: "The Marvel Age of Comics 1961–1978. 40th Ed",
+        Text: "Welcome to the Marvel Age of Comics, a triumphant era of comic and pop culture innovation which redefined the super hero genre."
+    },
+    {
+        url: "./images/image1.jpg",
+        name: "The Marvel Age of Comics 1961–1978. 40th Ed",
+        Text: "Welcome to the Marvel Age of Comics, a triumphant era of comic and pop culture innovation which redefined the super hero genre."
+    },
+    {
+        url: "./images/image1.jpg",
+        name: "The Marvel Age of Comics 1961–1978. 40th Ed",
+        Text: "Welcome to the Marvel Age of Comics, a triumphant era of comic and pop culture innovation which redefined the super hero genre."
+    }
 ]
 
 function createCard () {
-    let i = 0; cardData.length > i; i++
-    console.log(i)
     cardSection.insertAdjacentHTML('afterbegin', `
     <li class="card">
-        <img class="card__image" src="${cardData[i].URL}" alt="Comic Image">
+        <img class="card__image" src="${cardData[i].url}" alt="${cardData[i].name}">
         <div class="card__block">
             <h2 class="card__title">${cardData[i].name}</h2>
             <p class="card__text">${cardData[i].Text}</p>
@@ -70,7 +70,7 @@ function createCard () {
 `);
 }
 
-for (let i = 0; cardData.length > i; i++) {
+for (i = 0; cardData.length > i; i++) {
     createCard(cardData);
     console.log(i)
 };
