d172d6175ce8416a4b9f74e3329468d130dd4586
p00rt <serzhooo@gmail.com>
2021-08-25 15:58:04 +0300

    js code fixed

--- script/index.js
+++ script/index.js
@@ -1,168 +1,116 @@
-const cards = [
+const initialCards = [
     {
-        "title" : "The Marvel Age of Comics 1961–1978. 40th Ed",
-        "text" : "Welcome to the Marvel Age of Comics, a triumphant era of comic and pop culture innovation which redefined the super hero genre.",
-        "url" : "./images/image1.jpg",
+        name: "The Marvel Age of Comics 1961–1978. 40th Ed",
+        text: "Welcome to the Marvel Age of Comics, a triumphant era of comic and pop culture innovation which redefined the super hero genre.",
+        link: "./images/image1.jpg",
     },
     {
-        "title" : "Walt Disney's Mickey Mouse. The Ultimate History. 40th Ed.",
-        "text" : "Celebrate 90+ years of Mickey Mouse with one of the most expansive illustrated publications on the Disney universe: behind-the-scenes shots, rare animation art, and vintage comics trace Mickey’s cartoons, his comic adventures, the world of Mickey merchandise and memorabilia, as well as the legendary Mickey Mouse Club.",
-        "url" : "./images/image2.jpg",
+        name: "The Marvel Age of Comics 1961–1978. 40th Ed",
+        text: "Welcome to the Marvel Age of Comics, a triumphant era of comic and pop culture innovation which redefined the super hero genre.",
+        link: "./images/image1.jpg",
     },
     {
-        "title" : "The Golden Age of DC Comics",
-        "text" : "Get the ultimate insights on the heyday of DC Comics. Spanning from 1935 to 1956, more than 600 pages of covers and interiors, original illustrations, photographs, film stills, and ephemera chart the creators, the stories, and the game-changing super heroes Superman, Batman, and Wonder Woman.",
-        "url" : "./images/image3.jpg",
+        name: "The Marvel Age of Comics 1961–1978. 40th Ed",
+        text: "Welcome to the Marvel Age of Comics, a triumphant era of comic and pop culture innovation which redefined the super hero genre.",
+        link: "./images/image1.jpg",
     },
     {
-        "title" : "The Little Book of Captain America",
-        "text" : "Given an experimental super-serum, Steve Rogers became a super soldier who stands for liberty.",
-        "url" : "./images/image4.jpg",
+        name: "The Marvel Age of Comics 1961–1978. 40th Ed",
+        text: "Welcome to the Marvel Age of Comics, a triumphant era of comic and pop culture innovation which redefined the super hero genre.",
+        link: "./images/image1.jpg",
     }
-]
+];
 
-const cardContainer = document.querySelector('.section')
-for (let i = 0; i < cards.length; i++) {
-    titleValueHC = cards[i].title;
-    textValueHC = cards[i].text;
-    urlValueHC = cards[i].url;
-    const cardTemplate = document.querySelector('#card__template_hardcode').content;
-    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-    cardElement.querySelector('.card__title').textContent = titleValueHC
-    cardElement.querySelector('.card__text').textContent = textValueHC
-    cardElement.querySelector('.card__image').setAttribute("src", urlValueHC)
-    cardContainer.append(cardElement)
-}
+// Темплейты
+const cardTemplate = document.querySelector('#card__template')
+    .content.querySelector('.card__item');
+   
+// Врапперы
+const placesWrap = document.querySelector('.cards__list');
 
+// Кнопки и прочие дом узлы
+const cardFormModalWindow = document.querySelector('.popup');
+const closeCardFormButton = cardFormModalWindow.querySelector('.popup__button_close');
+const openCardFormButton = document.querySelector('.add-button');
 
+// Данные форм и элементы форм
+const cardNameInputValue = cardFormModalWindow.querySelector('.popup__input_type_card-name');
+const cardLinkInputValue = cardFormModalWindow.querySelector('.popup__input_type_url');
+const cardTextInput = cardFormModalWindow.querySelector('.popup__input_type_text');
 
-let popupBtn = document.querySelector('.add-button');
-let popupBtnClose = document.querySelector('.popup__button_close');
+const getCardElement = (data) => {
+    const cardElement = cardTemplate.cloneNode(true);
+    const likeButton = cardElement.querySelector('.card__like-button');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    const cardImage = cardElement.querySelector('.card__image');
+    // Можно лучше: переменная imageElement объявлена выше
+    cardImage.style.backgroundImage = `url(${data.link})`;
+    // Нужно исправить: Поиск одного и того же DOM-элемента не должен происходить дважды.
+    // Можно лучше: использовать интерполяцию строк из ES6
+    cardElement.querySelector('.card__title').textContent = data.name;
+    cardElement.querySelector('.card__text').textContent = data.text;
 
-function openPopup () {
-    document.querySelector(".popup").style.cssText = "display: flex;"
-    document.querySelector("body").style.cssText = "overflow: hidden;"
-}
+    likeButton.addEventListener('click', handleLikeIcon);
+    deleteButton.addEventListener('click', handleDeleteCard);
+    cardImage.addEventListener('click', () => handlePreviewPicture(data));
+    // До вставки в разметку добавляем слушатели событий.
+    return cardElement;
+  };
 
-function closePopup (popUp) {
-    document.querySelector(".popup").style.display = "none";
-    document.querySelector("body").style.cssText = "overflow: visible;"
-    document.querySelector('.popup__form').reset()
-}
+  const toggleModalWindow = (modalWindow) => {
+    // Нужно исправить - Функция выполняет только одну задачу
+    // Можно лучше - исапользовать для функции для открытия и закрытия: openPopup, closePopup
+    modalWindow.classList.toggle('popup_is-opened');
+  };
 
-popupBtn.addEventListener('click', openPopup);
-popupBtnClose.addEventListener('click', closePopup);
+  const handleLikeIcon = (evt) => {
+    evt.target.classList.toggle('card__like-button_is-active');
+  };
 
+  const handlePreviewPicture = (data) => {
+    imageElement.src = data.link;
+    imageElement.alt = `Изображение ${data.name}`;
+    // Можно лучше: использовать интерполяцию строк из ES6
+    // Сделано исключительно для г-на Семена Муравьева. От студентов этого не требуем.
+    imageCaption.textContent = data.name;
+    toggleModalWindow(imageModalWindow);
+  };
 
-
-function formSubmitHandler(evt) {
+  const handleDeleteCard = (evt) => {
+    evt.target.closest('.card').remove();
+  };
+  
+  const handleCardFormSubmit = (evt) => {
+    // Аналогично кнопке выше.
     evt.preventDefault();
-    closePopup ()
-}
+    // Можно лучше - не давать добавлять с пустыми полями карточку. (Не js валидация!)
+    renderCard({
+      name: cardNameInputValue.value,
+      text: cardTextInput.value,
+      link: cardLinkInputValue.value
+    }, placesWrap);
+    toggleModalWindow(cardFormModalWindow);
+    // Можно лучше - очищать форму после добавления новой карточки
+  };
+  
+  const renderCard = (data, wrap) => {
+    // Если в эту функцию вкладывает .forEach, то это не ок
+    // Функция renderCard должна переиспользовать при работе с формой сабмита новой карточки.
+    wrap.prepend(getCardElement(data));
+  };
+  
+  // EventListeners
+  cardFormModalWindow.addEventListener('submit', handleCardFormSubmit);
+  
+  openCardFormButton.addEventListener('click', () => {
+    toggleModalWindow(cardFormModalWindow);
+  });
+  
+  closeCardFormButton.addEventListener('click', () => {
+    toggleModalWindow(cardFormModalWindow);
+  });
 
-function createCard (titleValue, textValue, urlValue) {
-    const cardTemplate = document.querySelector('#card__template').content;
-    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-    cardElement.querySelector('.card__title').textContent = titleValue
-    cardElement.querySelector('.card__text').textContent = textValue
-    cardElement.querySelector('.card__image').setAttribute("src", urlValue)
-    cardContainer.append(cardElement)
-    
-    const likeBtn = document.querySelector('.card__button');
-    likeBtn.addEventListener('click', function (event) {
-        event.target.classList.toggle('card__button_active')
-    })
-
-    const trashBtn = document.querySelector('.trash');
-    trashBtn.addEventListener('click', function (event) {
-        const exB = event.target.parentNode
-        const exBB = exB.parentNode
-        const exBBB = exBB.parentNode
-        exBBB.parentElement.removeChild(exBBB)
-    })
-
-    document.querySelector('.popup__form').reset()
-    
-}
-
-const formElement= document.querySelector(".popup__content")
-formElement.addEventListener('submit', formSubmitHandler)
-
-formElement.addEventListener('submit', function () {
-    const title = document.querySelector('.popup__input_name').value
-    const text = document.querySelector('.popup__input_text').value
-    const url = document.querySelector('.popup__input_url').value
-
-    createCard(title, text, url)
-})
-
-
-
-let likeBtnHC = document.querySelectorAll('.card__button_hardcode');
-likeBtnHC.forEach((element) => {
-    element.addEventListener('click', function () {
-        let attribute = element.getAttribute('class');
-            if (attribute === 'card__button_hardcode') {
-                element.classList.add('card__button_hardcode_active');
-            } else {
-                element.classList.remove('card__button_hardcode_active');
-            }
-    })
-});
-
-const trashBtnHC = document.querySelectorAll('.trash__hardcode');
-trashBtnHC.forEach((element) => {
-    element.addEventListener('click', function () {
-        const exP = element.parentNode
-        const exPP = exP.parentNode
-        const exPPP = exPP.parentNode
-        exPPP.parentElement.removeChild(exPPP)
-    })
-});
-
-
-
-// function cardDataPush () {
-//     cardData.push(document.querySelector(".popup__input_text").value);
-//     console.log(cardData);
-//     return false;
-// }
-
-// let trashBtnHC = document.querySelectorAll('.trash_hardcode');
-
-// trashBtnHC.forEach((element) => {
-//     element.addEventListener('click', function () {
-//         let attribute = element.getAttribute('class');
-//             if (attribute === 'card__button_hardcode') {
-//                 element.classList.add('card__button_hardcode_active');
-//             } else {
-//                 element.classList.remove('card__button_hardcode_active');
-//             }
-//     })
-// }); 
-
-
-// trashBtn.addEventListener('click', function (event) {
-//     const sectionElement = document.querySelector('.section')
-//     const exP = event.target.parentNode
-//     const exPP = exP.parentNode
-//     const exPPP = exPP.parentNode
-//     console.log(exPPP)
-//     exPPP.target.sectionElement.removeChild(exPPP)
-// })
-
-// let delPopupBtn = document.querySelector('.trash');
-// let delPopupBtnClose = document.querySelector('.trash__close');
-
-// function openDelPopup () {
-//     document.querySelector(".trash__popup").style.cssText = "display: flex;"
-//     document.querySelector("body").style.cssText = "overflow: hidden;"
-// }
-
-// function closeDelPopup (popUp) {
-//     document.querySelector(".trash__popup").style.display = "none";
-//     document.querySelector("body").style.cssText = "overflow: visible;"
-// }
-
-// delPopupBtn.addEventListener('click', openDelPopup);
-// delPopupBtnClose.addEventListener('click', closeDelPopup);
+  // Render
+  initialCards.forEach((data) => {
+    renderCard(data, placesWrap)
+  });
