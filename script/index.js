75894d41d473ffd22b96243ef64ad2fd4ecf29df
todoslosfrijoles <86227547+todoslosfrijoles@users.noreply.github.com>
2021-08-26 12:53:11 +0300

    Summer-Final

--- script/index.js
+++ script/index.js
@@ -5,19 +5,19 @@ const initialCards = [
         link: "./images/image1.jpg",
     },
     {
-        name: "The Marvel Age of Comics 1961–1978. 40th Ed",
-        text: "Welcome to the Marvel Age of Comics, a triumphant era of comic and pop culture innovation which redefined the super hero genre.",
-        link: "./images/image1.jpg",
+        name: "Walt Disney's Mickey Mouse. The Ultimate History. 40th Ed.",
+        text: "Celebrate 90+ years of Mickey Mouse with one of the most expansive illustrated publications on the Disney universe: behind-the-scenes shots, rare animation art, and vintage comics trace Mickey’s cartoons, his comic adventures, the world of Mickey merchandise and memorabilia, as well as the legendary Mickey Mouse Club.",
+        link: "./images/image2.jpg",
     },
     {
-        name: "The Marvel Age of Comics 1961–1978. 40th Ed",
-        text: "Welcome to the Marvel Age of Comics, a triumphant era of comic and pop culture innovation which redefined the super hero genre.",
-        link: "./images/image1.jpg",
+        name: "The Golden Age of DC Comics",
+        text: "Get the ultimate insights on the heyday of DC Comics. Spanning from 1935 to 1956, more than 600 pages of covers and interiors, original illustrations, photographs, film stills, and ephemera chart the creators, the stories, and the game-changing super heroes Superman, Batman, and Wonder Woman.",
+        link: "./images/image3.jpg",
     },
     {
-        name: "The Marvel Age of Comics 1961–1978. 40th Ed",
-        text: "Welcome to the Marvel Age of Comics, a triumphant era of comic and pop culture innovation which redefined the super hero genre.",
-        link: "./images/image1.jpg",
+        name: "The Little Book of Captain America",
+        text: "Given an experimental super-serum, Steve Rogers became a super soldier who stands for liberty.",
+        link: "./images/image4.jpg",
     }
 ];
 
@@ -32,8 +32,10 @@ const placesWrap = document.querySelector('.cards__list');
 const cardFormModalWindow = document.querySelector('.popup');
 const closeCardFormButton = cardFormModalWindow.querySelector('.popup__button_close');
 const openCardFormButton = document.querySelector('.add-button');
+const imageElement = cardTemplate.querySelector('.card__image');
 
 // Данные форм и элементы форм
+const popupForm = cardFormModalWindow.querySelector('.popup__form')
 const cardNameInputValue = cardFormModalWindow.querySelector('.popup__input_type_card-name');
 const cardLinkInputValue = cardFormModalWindow.querySelector('.popup__input_type_url');
 const cardTextInput = cardFormModalWindow.querySelector('.popup__input_type_text');
@@ -43,8 +45,9 @@ const getCardElement = (data) => {
     const likeButton = cardElement.querySelector('.card__like-button');
     const deleteButton = cardElement.querySelector('.card__delete-button');
     const cardImage = cardElement.querySelector('.card__image');
-    // Можно лучше: переменная imageElement объявлена выше
-    cardImage.style.backgroundImage = `url(${data.link})`;
+
+    cardImage.src = `${data.link}`;
+
     // Нужно исправить: Поиск одного и того же DOM-элемента не должен происходить дважды.
     // Можно лучше: использовать интерполяцию строк из ES6
     cardElement.querySelector('.card__title').textContent = data.name;
@@ -67,14 +70,13 @@ const getCardElement = (data) => {
     evt.target.classList.toggle('card__like-button_is-active');
   };
 
-  const handlePreviewPicture = (data) => {
-    imageElement.src = data.link;
-    imageElement.alt = `Изображение ${data.name}`;
-    // Можно лучше: использовать интерполяцию строк из ES6
-    // Сделано исключительно для г-на Семена Муравьева. От студентов этого не требуем.
-    imageCaption.textContent = data.name;
-    toggleModalWindow(imageModalWindow);
-  };
+//   const handlePreviewPicture = (data) => {
+//     imageElement.src = data.link;
+//     imageElement.alt = `Изображение ${data.name}`;
+    
+//     imageCaption.textContent = data.name;
+//     toggleModalWindow(imageModalWindow);
+//   };
 
   const handleDeleteCard = (evt) => {
     evt.target.closest('.card').remove();
@@ -104,6 +106,8 @@ const getCardElement = (data) => {
   
   openCardFormButton.addEventListener('click', () => {
     toggleModalWindow(cardFormModalWindow);
+    popupForm.reset();
+
   });
   
   closeCardFormButton.addEventListener('click', () => {
